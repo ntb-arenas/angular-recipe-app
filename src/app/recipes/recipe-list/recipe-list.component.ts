e7dc566913c092c99af5f30455c5ef4c0f1f8ecd
Niño Arenas <ntbarenas@gmail.com>
2024-03-28 16:59:13 +0000

    Update recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.scss'
+  styleUrl: './recipe-list.component.scss',
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    {
+      name: 'A test Recipe',
+      description: 'This is simple a test',
+      imagePath:
+        'https://cdn.stocksnap.io/img-thumbs/960w/food-recipe_G8QICMKLUV.jpg',
+    },
+    {
+      name: 'A test Recipe',
+      description: 'This is simple a test',
+      imagePath:
+        'https://cdn.stocksnap.io/img-thumbs/960w/food-recipe_G8QICMKLUV.jpg',
+    },
+  ];
 
+  constructor() {
+    console.log(this.recipes);
+  }
 }
