e3ca16bbbae0fbbdac10d14e9571146c8f33a4a9
Niño Arenas <ntbarenas@gmail.com>
2024-03-28 17:36:57 +0000

    Update shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.scss'
+  styleUrl: './shopping-list.component.scss',
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    {
+      name: 'Eggs',
+      amount: 3,
+    },
+    {
+      name: 'Tomatoes',
+      amount: 10,
+    },
+  ];
 
+  constructor() {}
 }
