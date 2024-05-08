a93b80921a5fab30d54454ce00916c6c4d6823e6
Niño Arenas <ntbarenas@gmail.com>
2024-05-08 09:33:55 +0100

    Updates

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from "@angular/core";
+import { Ingredient } from "../../shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.scss'
+  selector: "app-shopping-edit",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrl: "./shopping-edit.component.scss",
 })
 export class ShoppingEditComponent {
+  @Output() ingredientsCreated = new EventEmitter<Ingredient>();
 
+  @ViewChild("nameInput") nameIngredientInput: ElementRef;
+  @ViewChild("amountInput") amountIngredientInput: ElementRef;
+
+  onAddIngredient() {
+    this.ingredientsCreated.emit({
+      name: this.nameIngredientInput.nativeElement.value,
+      amount: this.amountIngredientInput.nativeElement.value,
+    });
+  }
+
+  onClearIngredient() {
+    this.nameIngredientInput.nativeElement.value = "";
+    this.amountIngredientInput.nativeElement.value = "";
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
+import { Component } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.scss',
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrl: "./shopping-list.component.scss",
 })
 export class ShoppingListComponent {
   ingredients: Ingredient[] = [
     {
-      name: 'Eggs',
+      name: "Eggs",
       amount: 3,
     },
     {
-      name: 'Tomatoes',
+      name: "Tomatoes",
       amount: 10,
     },
   ];
+
+  onIngredientsCreated(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 
   constructor() {}
 }
